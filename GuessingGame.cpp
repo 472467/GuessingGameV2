c0ecab1f7a8c69429c465387b568f16c42513518
Brendan <472467@gmail.com>
2016-09-09 13:51:11 -0700

    Added comments and if statements

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -7,13 +7,29 @@ using namespace std;
 int main()
 {
   int secretNum, guessNum;
-  guessNum = -1;
-  srand (time(NULL));
-  
+  guessNum = -1;//makes sure while loop always runs
+
+  srand (time(NULL));//intializes the rand
+
+  secretNum = rand()%100;//rand 0-100
+
   cout << "Guess the correct number between 0-100.";
-  while (guessNum != secretNum){
-    
-    cin >> input;
+
+  while (guessNum != secretNum){//loops til number is found
+
+    cin >> input;//gets input
+    if(guessNum != secretNum){//incorrect guess
+
+      cout << "Incorrect guess!";
+      if(guessNum > secretNum){//guess bigger
+
+	  cout << "The guess was larger than the secret number";
+	}else{//guess smaller
+
+	  cout << "The guess was smaller than the secret number";
+	}
+    }
+    if(guessNum
   }
   return 0;
 }
