8a56c528149121c12c2231281c66c2d8008be95b
Brendan <472467@gmail.com>
2016-09-09 13:39:25 -0700

    Commit 1

---
+++ GuessingGame.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+  int secretNum, guessNum;
+  guessNum = -1;
+  srand (time(NULL));
+  
+  cout << "Guess the correct number between 0-100.";
+  while (guessNum != secretNum){
+    
+    cin >> input;
+  }
+  return 0;
+}
